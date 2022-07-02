81fc98e0c416861079271293eca43e9384a787f7
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-07-02 23:05:33 +0100

    Added helmet and cors to the middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 // Import all routes
