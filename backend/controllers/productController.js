71501795e3615acd203ce4d124ed45c4cfa81a3f
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-07-02 12:54:59 +0100

    Added an API route to get product by it's ID

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,9 +11,32 @@ exports.newProduct = async (req, res, next) => {
   });
 };
 
-exports.getProducts = (req, res, next) => {
+// Get all the products from the database => api/v1/products
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
+
   res.status(200).json({
     success: true,
-    message: "This route will display all the products in the database",
+    count: products.length,
+    products,
   });
+};
+
+// Get specific product by it's id => api/v1/product/:id
+exports.getProductByID = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+    console.log(`Product with id: ${req.params.id} was successfully found!`);
+  } catch (err) {
+    console.log(`Product with id: ${req.params.id} was not found!`);
+    return res.status(404).json({
+      success: false,
+      message: "Product doeas not exist!",
+    });
+  }
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,12 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getProducts, newProduct } = require("../controllers/productController");
+const {
+  getProducts,
+  newProduct,
+  getProductByID,
+} = require("../controllers/productController");
 
 // All Products router
 router.route("/products").get(getProducts);
 
 // New Product router
 router.route("/product/new").post(newProduct);
+
+// Product by id router
+router.route("/product/:id").get(getProductByID);
 
 module.exports = router;
